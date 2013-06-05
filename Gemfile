12cffaa51eae77458b1604df29fdbd6618386db8
Reza Khadjavi <reza@gmail.com>
2013-06-05 00:17:35 -0400

    Make a basic user bodel (including secure passwords)

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 gem 'rails', '3.2.13'
 gem 'bootstrap-sass', '2.0.0'
 gem 'pg', '0.12.2'
+gem 'bcrypt-ruby', '3.0.1'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
@@ -10,7 +11,7 @@ gem 'pg', '0.12.2'
 group :development, :test do
   
   gem 'rspec-rails', '2.9.0'
-  
+  gem 'annotate', '~> 2.4.1.beta'
 end
 
 gem 'thin'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130602230306) do
+ActiveRecord::Schema.define(:version => 20130604165101) do
 
   create_table "articles", :force => true do |t|
     t.string   "name"
@@ -22,9 +22,12 @@ ActiveRecord::Schema.define(:version => 20130602230306) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
-    t.text     "content"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.string   "email"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.string   "password_digest"
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
