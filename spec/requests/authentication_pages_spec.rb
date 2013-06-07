5be5b06b8d7af2866fb82bb79861cd0250851498
Reza Khadjavi <reza@gmail.com>
2013-06-07 12:53:24 -0400

    Finish user edit, update, index and destroy actions

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -13,6 +13,7 @@ describe "Authentication" do
     before { visit signin_path }
 
     describe "with invalid information" do
+      
       before { click_button "Sign in" }
 
       it { should have_selector('title', text: 'Sign in') }
